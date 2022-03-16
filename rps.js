03c6e77a18a4e16736c467529af09f568f94bca3
Derrick Maurais <derrickmaurais1@gmail.com>
2022-03-16 20:59:59 -0300

    simplified the game a bit. trying to figure out how to get a pop up prmpt to reset the gam\

--- rps.js
+++ rps.js
@@ -11,61 +11,85 @@
 
 let playerScore = 0
 let compScore = 0
+let roundWinner = ""
+let gameOver = false
 
 
 function playRound(playerSelection, computerSelection) {
       let comchoice = ['rock', 'paper', 'scissors'];
       playerSelection = playerChoice
       computerSelection = comchoice[Math.floor(Math.random() * comchoice.length)];
-      let lose = computerSelection + ', You lose!'
-      let win = computerSelection + ', You win!'
       console.log(computerSelection)
       console.log(playerSelection)
       if (playerSelection === computerSelection) {
-            console.log("Draw!");
-      } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            console.log(lose);
-            compScore++
-      } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            console.log(win);
-            playerScore++
-      } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            console.log(win);
-            playerScore++
-      } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            console.log(lose);
-            compScore++
-      } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            console.log(lose);
-            compScore++
-      } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            console.log(win);
-            playerScore++
-      } else {
-            alert("that is an invalid choice");
+            roundWinner = 'tie'
       }
-      console.log(playerScore)
-      console.log(compScore)
-}
+      if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'rock')
+      ) {
+            playerScore++
+            roundWinner = 'player'
+            console.log(playerScore, "p")
+      }
+      if (
+            (computerSelection === 'rock' && playerSelection === 'scissors') ||
+            (computerSelection === 'scissors' && playerSelection === 'paper') ||
+            (computerSelection === 'paper' && playerSelection === 'rock')
+      ) {
+            compScore++
+            roundWinner = 'computer'
+            console.log(compScore, "c")
+      }
+      
+
 // getting ahold of html elements.
 const head = document.getElementById('head');
-const title = document.getElementById('title');
-const subtitle = document.getElementById('subtitle');
-const gameSpot = document.getElementById('gameSpot');
-const choiceBox = document.getElementById('choiceBox');
-const playerdisp = document.getElementById('playerDisp');
-const computerdisp = document.getElementById('computerChoice')
-const pScore = document.getElementById('pScore');
-const cScore = document.getElementById('cScore');
+const buttonBox = document.getElementById("buttonBox");
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
+// using buttons to get user input then trigger a round of the game
+rock.addEventListener('click', () => {
+      playerChoice = "rock";
+      if (playerScore == 5 || compScore == 5) {
+            if (playerScore < compScore){
+                  alert(<button onClick="window.location.reload();"> You lose, play again?</button>)
+            }else {
+                  alert(<button onClick="window.location.reload();">You win! Play again?</button>)
+            }
+      } else {
+            playRound();
 
-rock.addEventListener('click', () => { playerChoice = "rock", playRound()});
-paper.addEventListener('click', () => { playerChoice = "paper", playRound()});
-scissors.addEventListener('click', () => { playerChoice = "scissors", playRound()});
+
+      }
+});
+paper.addEventListener('click', () => {
+      playerChoice = "paper";
+      if (playerScore == 5 || compScore == 5) {
+            if (playerScore < compScore){
+                  alert(<button onClick="window.location.reload();"> You lose, play again?</button>)
+            }else {
+                  alert(<button onClick="window.location.reload();">You win! Play again?</button>)
+            }
+      } else {
+            playRound();
+      }
+});
+scissors.addEventListener('click', () => {
+      playerChoice = "scissors";
+      if (playerScore == 5 || compScore == 5) {
+            if (playerScore < compScore){
+                  alert(<button onClick="window.location.reload();"> You lose, play again?</button>)
+            }else {
+                  alert(<button onClick="window.location.reload();">You win! Play again?</button>)
+            }
+      } else {
+            playRound();
+      }
+});
 
 
-// 
 
